924a4d131760b808fe12fbe7cc68330d8bb12ab2
Dan J Hamilton <danhamilton1964@gmail.com>
2018-07-25 00:14:04 -0400

    Modify Exception Handling
    Adam Watham modifications

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use App\User;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -10,11 +12,46 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+//        Schema::enableForeignKeyConstraints();
+//
+        $this->disableExceptionHandling();
+    }
+
     protected function signIn($user = null)
     {
         $user = $user ?: create(User::class);
 
         $this->actingAs($user);
+
+        return $this;
+    }
+
+    // Hat tip, @adamwathan.
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct()
+            {
+            }
+            public function report(\Exception $e)
+            {
+            }
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
 
         return $this;
     }
