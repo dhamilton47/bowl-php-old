cfe58576c14b42fe43e76bf25f84f2514c5ea935
Dan J Hamilton <danhamilton1964@gmail.com>
2018-07-24 23:18:57 -0400

    Add footer
    TSK Copyright

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,14 @@
 
     <style media="screen">
         body { padding-bottom: 100px; }
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px; /* Set the fixed height of the footer here */
+            line-height: 60px; /* Vertically center the text there */
+            background-color: #f5f5f5;
+        }
         .level { display: flex; align-items: center; }
         .flex { flex: 1; }
         .mr-1 { margin-right: 1em; }
@@ -46,6 +54,8 @@
 
         <flash message="{{ session('flash') }}"></flash>
     </div>
+
+    @include ('layouts.footer')
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        &copy; 2018 TSK Solutions.  All rights reserved.
+    </div>
+</footer>
